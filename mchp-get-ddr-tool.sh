bb9acb18c2b6f6606f6c1e33857021c8f7f25a13
Torben Falkenberg <torben.falkenberg@microchip.com>
2023-12-15 15:43:41 +0100

    Updated mchp-get-xxx.sh scripts

---
+++ mchp-get-ddr-tool.sh
@@ -0,0 +1,50 @@
+
+# (1) --- Checkout the ddr tool code, if it does not already exist.
+#
+if [ ! -e ddr-umctl ]; then
+    git clone https://github.com/microchip-ung/ddr-umctl.git
+fi
+
+# (2) --- Connect to the arm-trusted-firmware-xxxx folser, that
+#         mch-get-tfa.sh has created. You may need to adjust
+#         $RELEASE below, the that it match arm-trusted-firmware-$RELEASE
+#
+RELEASE=2.8.8-mchp0
+
+if [ ! -e arm-trusted-firmware-$RELEASE ]; then
+
+
+    echo "The folder arm-trusted-firmware-$RELEASE does not exist."
+    echo "So adjust the   \$RELEASE=$RELEASE   variable in this script"
+
+else 
+
+    
+    DDR_PATH_ORG=arm-trusted-firmware-$RELEASE/plat/microchip/lan966x/common
+
+    DDR_CONFIG_NEW=my-lan966x_ddr_config.c
+    DDR_CONFIG_ORG=$DDR_PATH_ORG/lan966x_ddr_config.c
+
+
+    # (3) --- Build DDR configuration file for LAN966x
+    #
+    cd ddr-umctl
+    ./scripts/gen_cfg.rb -f source configs/profiles/lan966x.yaml > $DDR_CONFIG_NEW
+    cd ..
+    
+    # (4) --- Check the difference between 
+    diff  ddr-umctl/$DDR_CONFIG_NEW  $DDR_CONFIG_ORG
+    
+    if [ "false" = "true" ]; then
+	
+	# (5) --- If you think my-lan966x_ddr_config.c is ok, then replace the lan966x_ddr_config.c with it
+	cp  ddr-umctl/$DDR_CONFIG_NEW  $DDR_CONFIG_ORG
+	
+	# (6) --- Runstep (2) again
+	cd sw-arm-trusted-firmware-$RELEASE
+	dr ./script/build.rb -p lan966x_b0
+
+    fi
+
+    
+fi
--- mchp-get-tfa.sh
+++ mchp-get-tfa.sh
@@ -11,9 +11,23 @@
 #
 # (1) --- get the ATF / TF-A tool
 #
-wget https://github.com/microchip-ung/arm-trusted-firmware/archive/refs/tags/mchp_v1.0.5.tar.gz
-tar xzf mchp_v1.0.5.tar.gz
-cd arm-trusted-firmware-mchp_v1.0.5
+#         You can enable the release you need, if the latest is not what you want.
+
+#RELEASE=mchp_v1.0.5
+#RELEASE_=$RELEASE
+
+#RELEASE=mchp_v1.0.6
+#RELEASE_=$RELEASE
+
+#RELEASE=2.6-mchp0
+#RELEASE_=v$RELEASE
+
+RELEASE=2.8.8-mchp0
+RELEASE_=v$RELEASE
+
+wget https://github.com/microchip-ung/arm-trusted-firmware/archive/refs/tags/$RELEASE_.tar.gz
+tar xzf $RELEASE_.tar.gz
+cd arm-trusted-firmware-$RELEASE
 
 
 # (2) --- Build the fip image.
@@ -22,7 +36,7 @@ dr ./scripts/build.rb -p lan966x_b0
 
 
 # Note: If you have build your own U-Boot from the source BSP, lets call it u-boot.bin,
-#  then copy it to the arm-trusted-firmware-mchp_v1.0.5 folder and run
+#  then copy it to the arm-trusted-firmware-$RELEASE folder and run
 #
 #   $ dr ./scripts/build.rb -p lan966x_b0 --bl33-blob u-boot.bin
 #
